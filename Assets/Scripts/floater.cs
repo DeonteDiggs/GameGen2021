6f67a19f5289331be8a52287f9afd5e4f45a348e
DESKTOP-CMJ92UP\Kim <kim.viens2@gmail.com>
2021-05-12 19:30:50 -0300

    The sphere object now floats!

---
+++ Assets/Scripts/floater.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floater : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    public float depthBeforeSubmerged = 1f;
+    public float displacementAmount = 3f;
+
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void FixedUpdate()
+    {
+        float waveHeight = waveManager.instance.getWaveHeight(transform.position.x);
+
+        if( transform.position.y < waveHeight ){
+            float displacementMultiplier = Mathf.Clamp01(waveHeight - transform.position.y / depthBeforeSubmerged)*displacementAmount;
+            rigidbody.AddForce(new Vector3(0f, Mathf.Abs(Physics.gravity.y)*displacementMultiplier, 0f), ForceMode.Acceleration);
+        }
+    }
+}
---
+++ Assets/Scripts/waveManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waveManager : MonoBehaviour
+{
+    public static waveManager instance;
+    public float amplitude = 1f;
+    public float length = 2f;
+    public float speed = 1f;
+    public float offset = 0f;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        if( instance == null ){
+            instance = this;
+        } else if( instance != this){
+            //destroy(this);
+        }
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        offset += Time.deltaTime*speed;
+    }
+
+    public float getWaveHeight(float _x){
+        return amplitude*Mathf.Sin(_x/length+offset);
+    }
+}
