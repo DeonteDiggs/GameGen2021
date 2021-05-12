a720966b2f90ead1e8d546fe7fa925bf7fd748d7
Parker <parker.verboom@gmail.com>
2021-05-12 12:36:15 -0300

    More work on windBlower, add a rock that reloads the scene when colliding with 'boat' tagged objects

---
+++ Assets/Scripts/targetObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class targetObject : MonoBehaviour
+{
+
+    public GameObject targetBoat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = targetBoat.transform.position;
+    }
+}
