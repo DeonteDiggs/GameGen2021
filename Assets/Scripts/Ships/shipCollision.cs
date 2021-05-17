67dd68084f1d8da9ef65256eed0e96991169771d
DESKTOP-CMJ92UP\Kim <kim.viens2@gmail.com>
2021-05-17 20:19:22 -0300

    Audio explosion works!

--- Assets/Scripts/Ships/shipCollision.cs
+++ Assets/Scripts/Ships/shipCollision.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 // This class handles collisions for the ships.
 public class shipCollision : MonoBehaviour
 {
+    private AudioSource audio;
+    private GameObject camera;
+    public AudioClip audioclip;
+
     void OnCollisionEnter(Collision hit) 
     {
+        audio = GetComponent<AudioSource>();
+    
         if (hit.gameObject.tag == "hazard") 
         {
+            camera = GameObject.FindGameObjectWithTag("MainCamera");
+            audio = camera.GetComponent<AudioSource>();
+            audio.PlayOneShot(audioclip, 1);
             // Remove itself from the ship list in gameplay manager
             gameplayManager.Instance.ships.Remove(gameObject);
             Destroy(gameObject);
