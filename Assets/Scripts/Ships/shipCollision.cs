7c19b1d06e5df83ef3de638c3988c2d08b82f56b
pgarg <pl201920@dal.ca>
2021-05-17 20:17:20 -0300

    Better collision added - Pulkit

--- Assets/Scripts/Ships/shipCollision.cs
+++ Assets/Scripts/Ships/shipCollision.cs
@@ -5,18 +5,23 @@ using UnityEngine;
 // This class handles collisions for the ships.
 public class shipCollision : MonoBehaviour
 {
-    void OnCollisionEnter(Collision hit) 
+    public GameObject exp;
+
+    void OnCollisionEnter(Collision hit)
     {
-        if (hit.gameObject.tag == "hazard") 
+        if (hit.gameObject.tag == "hazard")
         {
+            GameObject explosion = Instantiate(exp, transform.position, transform.rotation);
+            ParticleSystem.MainModule particle = explosion.GetComponent<ParticleSystem>().main;
             // Remove itself from the ship list in gameplay manager
             gameplayManager.Instance.ships.Remove(gameObject);
             Destroy(gameObject);
+            Destroy(explosion, particle.duration);
         }
     }
 
-    void OnDestroy() 
+    void OnDestroy()
     {
-        
+
     }
 }
