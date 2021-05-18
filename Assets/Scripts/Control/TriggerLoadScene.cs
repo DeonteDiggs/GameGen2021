6d03a8b37d6cf8e8c43494fba55c45b7e21c4968
Pedram Safari <peediram@gmail.com>
2021-05-18 13:50:17 -0300

    Lvl 1 transitions work

--- Assets/Scripts/Control/TriggerLoadScene.cs
+++ Assets/Scripts/Control/TriggerLoadScene.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 
 public class TriggerLoadScene : MonoBehaviour
 {
-    public string SceneName;
+    public string SceneName1;
+    public string SceneName2;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "boat")
         {
-            SceneManager.LoadScene(SceneName); // loads scene When player enter the trigger collider
+            SceneManager.LoadScene(SceneName1); // loads scene When player enter the trigger collider
+        }
+        if (other.tag == "Finish")
+        {
+            SceneManager.LoadScene(SceneName2); // loads scene When player enter the trigger collider
         }
     }
 }
